e071b23557cc950f8292aa16eaef1d164aee5492
albert-dev <al96@hotmail.es>
2026-08-13 19:38:39 +0200

    feat: "created routes for the different pages of the application"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+import { Movies } from './components/movies/movies';
+import { Ranking } from './components/ranking/ranking';
+import { UserProfile } from './components/user-profile/user-profile';
 
 export const routes: Routes = [
-    { path: '', component: }
+    { path: '', component: HomePage},
+    { path: 'Movies', component: Movies},
+    { path: 'Ranking', component: Ranking},
+    { path: 'Profile', component: UserProfile},
+    { path: '**', redirectTo: '', pathMatch: 'prefix'}
 ];
