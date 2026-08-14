cfc54a012b6924000181e92b54dae515cb1a2c9c
hectordev4 <125360015+hectordev4@users.noreply.github.com>
2026-08-14 11:13:35 +0200

    feat: api-service created with a mock view of it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MovieList } from './feature/movie-list/movie-list'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '', 
+        component: MovieList, 
+        pathMatch: 'full' 
+    },
+    { 
+        path: '**', 
+        redirectTo: '' 
+    }
+];
