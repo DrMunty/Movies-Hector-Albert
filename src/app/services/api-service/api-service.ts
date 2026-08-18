436ebb632068b2754f8124aa5bc9e92793ea90a4
albert-dev <al96@hotmail.es>
2026-08-18 17:07:11 +0200

    refactor: "created a new method called searchMulti so that the user can search multiple things from the navbar"

--- src/app/services/api-service/api-service.ts
+++ src/app/services/api-service/api-service.ts
@@ -6,17 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private http = inject(HttpClient); // Modern Angular dependency injection
+  private http = inject(HttpClient);
   
-  private apiUrl = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+  private baseUrl = 'https://api.themoviedb.org/3';
   private bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzM2E4NmY4MGY4OTgyNGUzZDMxOTdmOWFhMGUzYzAyYyIsIm5iZiI6MTc4NjQ0NjM1OC42Nywic3ViIjoiNmE3YjAyMTZkYjE5ZThkZGNlMzE5Y2FhIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.2xQxyO6auWyIkrNU4jUJsUmFbKyeg_QV41YEGR5WC2c';
-
-  getMain(): Observable<any> {
-    const headers = new HttpHeaders({
+  
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
       'accept': 'application/json',
       'Authorization': `Bearer ${this.bearerToken}`
     });
+  }
 
-    return this.http.get<any>(this.apiUrl, { headers });
+  getMain(): Observable<any> {
+    const url = `${this.baseUrl}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+    return this.http.get<any>(url, { headers: this.getHeaders() });
+  }
+
+searchMulti(query: string): Observable<any> {
+    const safeQuery = encodeURIComponent(query);
+    const url = `${this.baseUrl}/search/multi?query=${safeQuery}&include_adult=false&language=en-US&page=1`;
+    
+    return this.http.get<any>(url, { headers: this.getHeaders() });
   }
 }
