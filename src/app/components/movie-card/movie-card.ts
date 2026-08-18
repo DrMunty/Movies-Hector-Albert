6e6e2e88363dbe6569b765f19d16a6a46c3e036b
albert-dev <al96@hotmail.es>
2026-08-18 17:38:10 +0200

    feat: "created a movie interface and a movie card component to render the data from the API into the HTML"

---
+++ src/app/components/movie-card/movie-card.ts
@@ -0,0 +1,18 @@
+import { Component, input } from '@angular/core';
+import type { Movie } from '../../models/movie-interface';
+
+@Component({
+  selector: 'app-movie-card',
+  imports: [],
+  templateUrl: './movie-card.html',
+  styleUrl: './movie-card.css',
+})
+export class MovieCard {
+  movie = input.required<Movie>();
+  get posterUrl(): string {
+    const path = this.movie().poster_path;
+    return path 
+      ? `https://image.tmdb.org/t/p/w500${path}` 
+      : 'https://via.placeholder.com/500x750?text=No+Poster';
+  }
+}
