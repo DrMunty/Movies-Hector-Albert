7fef19d9461b5efafd47066e63b9de24898b2e97
hectordev4 <125360015+hectordev4@users.noreply.github.com>
2026-08-24 20:00:38 +0200

    clean: all components for the site cleaned from the branch

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { Navbar } from './components/navbar/navbar';
-import { MovieList } from './feature/movie-list/movie-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, MovieList, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
