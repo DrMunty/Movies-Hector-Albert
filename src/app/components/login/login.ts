b754e5cbb6ac1acb5fd012be77add881a4113715
albert-dev <al96@hotmail.es>
2026-09-02 12:22:23 +0200

    feat: "the user get's redirected to the correct page when clicking on login/sigin and fixed styles and text of the login page"

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,8 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '@services/auth/auth';
-import { RouterLink } from '@angular/router';
+import { RouterLink, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +10,10 @@ import { RouterLink } from '@angular/router';
   imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html'
 })
-export class Login {
+export class Login implements OnInit {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private route = inject(ActivatedRoute);
 
   isRegisterMode = signal<boolean>(false);
   errorMessage = signal<string | null>(null);
@@ -22,6 +23,16 @@ export class Login {
     email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(6)]]
   });
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if (params['mode'] === 'register') {
+        this.isRegisterMode.set(true);
+      } else {
+        this.isRegisterMode.set(false);
+      }
+    });
+  }
 
   toggleMode(): void {
     this.isRegisterMode.update(val => !val);
