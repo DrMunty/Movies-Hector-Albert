080c4d8c6cf0611404f3fc2b363a6dfe2e6ff13d
albert-dev <al96@hotmail.es>
2026-09-06 10:52:14 +0200

    feat: "created favorite-movies component to store user's favorite movies"

---
+++ src/app/components/favorite-movies/favorite-movies.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-favorite-movies',
+  styleUrl: './favorite-movies.css',
+  templateUrl: './favorite-movies.html',
+})
+export class FavoriteMovies {}
