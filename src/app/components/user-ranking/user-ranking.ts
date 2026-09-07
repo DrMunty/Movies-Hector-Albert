97682b108a5dfde813f2f0ba129028968f1442e0
albert-dev <al96@hotmail.es>
2026-09-07 12:45:59 +0200

    feat: "the user-ranking component now displays user's top rated and favorited movies"

--- src/app/components/user-ranking/user-ranking.ts
+++ src/app/components/user-ranking/user-ranking.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DbService } from '@services/database/database-service';
 
 @Component({
-  imports: [],
+  imports: [CommonModule, RouterModule],
   selector: 'app-user-ranking',
   styleUrl: './user-ranking.css',
   templateUrl: './user-ranking.html',
 })
-export class UserRanking {}
+export class UserRanking implements OnInit{
+  private dbService = inject(DbService);
+
+  rankings = signal<any[]>([]);
+  isLoading = signal<boolean>(true);
+  sortBy = signal<'favorites' | 'rating'>('favorites');
+
+  async ngOnInit() {
+    this.isLoading.set(true);
+    try {
+      const data = await this.dbService.getGlobalRankings();
+      this.rankings.set(data);
+      this.sortData('favorites');
+    } catch (error) {
+      console.error('Error cargando rankings:', error);
+    } finally {
+      this.isLoading.set(false);
+    }
+  }
+
+  sortData(criteria: 'favorites' | 'rating') {
+    this.sortBy.set(criteria);
+    const sorted = [...this.rankings()].sort((a, b) => {
+      if (criteria === 'favorites') {
+        return b.favoriteCount - a.favoriteCount || b.averageRating - a.averageRating;
+      } else {
+        return b.averageRating - a.averageRating || b.favoriteCount - a.favoriteCount;
+      }
+    });
+    this.rankings.set(sorted);
+  }
+}
+
