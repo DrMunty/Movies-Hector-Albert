7a0355762e1d3fa6cf39ec2c7e2788682d24b422
albert-dev <al96@hotmail.es>
2026-09-07 12:49:41 +0200

    feat: "created a route to the ranking page"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePage } from '@components/home-page/home-page';
 import { Movies } from '@components/movies-list/movies-list';
-// import { Ranking } from '@components/ranking/ranking';
-// import { UserProfile } from '@components/user-profile/user-profile';
 import { MovieCardDetails } from '@components/movie-card-details/movie-card-details';
 import { PersonDetails } from '@components/person-detail/person-detail';
 import { TvShowDetails } from '@components/tvshow-detail/tvshow-detail';
 import { Login } from '@components/login/login';
 import { FavoriteMovies } from '@components/favorite-movies/favorite-movies';
+import { UserRanking } from '@components/user-ranking/user-ranking';
 
 export const routes: Routes = [
     { path: '', component: HomePage},
@@ -17,5 +16,6 @@ export const routes: Routes = [
     { path: 'tv/:id', component: TvShowDetails},
     { path: 'login', component: Login},
     { path: 'user-movies', component: FavoriteMovies},
+    { path: 'user-ranking', component: UserRanking},
     { path: '**', redirectTo: '', pathMatch: 'prefix'}
 ];
